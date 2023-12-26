dcaf12a5bb6693508fc2b7698e6d4cd9d0c906b4
axel <axel@kippel.de>
2023-12-26 14:09:23 +0100

    added gitea

--- dopush.sh
+++ dopush.sh
@@ -14,7 +14,21 @@ git push
 
 sed -i -e 's/^#theme:/theme:/' -e 's/^remote_theme:/#remote_theme:/' _config.yml
 
-rsync -av . ../zubeax.gitea.io --exclude .git
+dest='../zubeax.gitea.io'
+
+rsync -av . $dest --exclude .git --exclude _site --exclude vendor --exclude .jekyll_cache
+
+for i in (_site vendor .jekyll_cache)
+do
+    rm -rf $dest/$i
+done
+
+pushd . 2>&1 > /dev/null
+cd $dest
+git add -A .
+git commit -m "$commitmessage"
+git push
+popd . 2>&1 > /dev/null
 
 exit 0
 
