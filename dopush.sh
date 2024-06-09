4fa25a91a86596b7cf72dba197b84bb3830c2089
axel <axel@kippel.de>
2024-06-09 21:43:19 +0200

    commit to gitea

--- dopush.sh
+++ dopush.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-gitearepo='../zubeax.gitea.io'
-
 commitmessage="$1"
 if [ x"$commitmessage" == x ]
 then
@@ -16,28 +14,14 @@ git commit -m "$commitmessage"
 git push
 }
 
-sed -i -e 's/^theme:/#theme:/' -e 's/^#remote_theme:/remote_theme:/' _config.yml
+sed -i -e 's/^#theme:/theme:/' -e 's/^remote_theme:/#remote_theme:/' _config.yml
 
 docommit
 
-sed -i -e 's/^#theme:/theme:/' -e 's/^remote_theme:/#remote_theme:/' _config.yml
-
-rsync -av . $gitearepo -f '- .git/' -f '- _site/' -f '- vendor/' -f '- .jekyll-cache/'
+sed -i -e 's/^theme:/#theme:/' -e 's/^#remote_theme:/remote_theme:/' _config.yml
 
 [[ -d $gitearepo/_site  ]] && { echo "unexpected dir $gitearepo/_site found"; exit -4; }
 [[ -d $gitearepo/vendor ]] && { echo "unexpected dir $gitearepo/vendor found"; exit -4; }
 
-##
-#   not strictly necessary since .gitignore prevents the caches from upload to the repo
-##
-for i in _site vendor .jekyll-cache
-do
-    rm -rf $dest/$i
-done
-
-pushd . 2>&1 > /dev/null
-cd $gitearepo
-docommit
-popd 2>&1 > /dev/null
 
 exit 0
