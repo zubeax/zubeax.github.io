3b5c8ae22d0f68168e1af9a44ede5c695b822049
axel <axel@kippel.de>
2024-06-16 23:27:42 +0200

    Added custom comments.

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -2,6 +2,6 @@ function spread(count){
     document.getElementById('folder-checkbox-' + count).checked =
     !document.getElementById('folder-checkbox-' + count).checked
     document.getElementById('spread-icon-' + count).innerHTML =
-    document.getElementById('spread-icon-' + count).innerHTML == 'arrow_right' ?
-    'arrow_drop_down' : 'arrow_right'
+    document.getElementById('spread-icon-' + count).innerHTML == 'density_small' ?
+    'unfold_less' : 'density_small'
 }
